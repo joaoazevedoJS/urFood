d62810c76f466b7015eb934c7cea3b3f037730b5
João Azevedo <59920486+joaoazevedoJS@users.noreply.github.com>
2020-06-18 15:22:52 -0300

    urFood, version - simple HTML - Menu animation and add contact page

--- html/js/script.js
+++ html/js/script.js
@@ -1,6 +1,25 @@
-for(let menu of document.querySelectorAll('.js-menu')) {
-  menu.addEventListener('click', () => {
+for (let menu of document.querySelectorAll('.js-menu')) {
+  if (menu.classList.value === 'close js-menu') {
+    menu.addEventListener('click', animetedMenu)
+  } else {
+    console.log(menu)
+    menu.addEventListener('click', () => {
+      document.querySelector('nav.menu')
+        .classList.toggle('hide')
+    })
+  }
+}
+
+function animetedMenu() {
+  document.querySelector('nav.menu section.container').animate([
+    { transform: 'translateX(0px)' },
+    { transform: 'translateX(300px)' }
+  ], {
+    duration: 200,
+  })
+
+  setTimeout(() => {
     document.querySelector('nav.menu')
       .classList.toggle('hide')
-  })
+  }, 200)
 }
